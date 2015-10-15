9061884f5d66b9de24bad068b203f904a84fb3f1
Rodrigo Delgado <rdalcega@gmail.com>
2015-10-15 07:46:19 -0700

    Add gulp jshint to developer dependencies.

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require( 'gulp' );
 var mocha = require( 'gulp-mocha' );
+var jshint = require( 'gulp-mocha' );
 
 gulp.task( 'test', function( ) {
   return gulp.src( './tests/unit/*-test.js' )
