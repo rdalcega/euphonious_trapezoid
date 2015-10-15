35e7580e78488708f3fc1a14d191a59e75f2c03b
Zachary T. Castellanos-Smith <ztsmith25@gmail.com>
2015-10-15 13:15:12 -0700

    add db connection file

---
+++ server/db/db.js
@@ -0,0 +1,11 @@
+var mysql = require('mysql');
+
+var pool = mysql.createPool({
+user     : 'root',
+password : '',
+database : 'Sphero',
+host: 'localhost'
+});
+
+// export the db connection
+module.exports = pool;
