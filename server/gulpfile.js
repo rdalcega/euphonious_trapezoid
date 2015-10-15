225eb1127434fe07afdec9ea5b26f98cfafaf770
Rodrigo Delgado <rdalcega@gmail.com>
2015-10-15 08:00:32 -0700

    Create lint gulp task using gulp-jshint and jshint-stylish

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -1,8 +1,17 @@
+'use strict';
+
 var gulp = require( 'gulp' );
 var mocha = require( 'gulp-mocha' );
-var jshint = require( 'gulp-mocha' );
+var jshint = require( 'gulp-jshint' );
+var stylish = require( 'jshint-stylish' );
 
 gulp.task( 'test', function( ) {
   return gulp.src( './tests/unit/*-test.js' )
              .pipe( mocha() );
+});
+
+gulp.task( 'lint', function( ) {
+  return gulp.src( './**.js' )
+             .pipe( jshint( ) )
+             .pipe( jshint.reporter( stylish ) );
 });
