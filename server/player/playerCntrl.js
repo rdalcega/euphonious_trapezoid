dae2b11ca2e3d1e64553896aea9baa5fd665b6bc
Zachary T. Castellanos-Smith <ztsmith25@gmail.com>
2015-10-15 18:29:29 -0700

    start setupe of player routes and controller methods

---
+++ server/player/playerCntrl.js
@@ -0,0 +1,7 @@
+var db = require('../db/db.js');
+
+module.exports.signup = function(req, res) {
+  //var sqlQuery = 'SELECT * FROM Players WHERE Player_ID =' + req.body.userName;
+
+
+};
---
+++ server/player/playerRoutes.js
@@ -0,0 +1,9 @@
+var playerController = require('./playerCtrnl');
+
+//links controller methods to respective routes
+module.exports = function(router){
+
+  router.post('/signup', playerController.signup);
+  router.get('/signin', playerController.signin);
+
+};
