1acc89b720877295ba959d3f2d0a3c06f2696e8d
Rodrigo Delgado <rdalcega@gmail.com>
2015-10-16 14:10:38 -0700

    Node insert works.

--- server/game/game.js
+++ server/game/game.js
@@ -1,13 +1,13 @@
 var Node = require('./node.js');
 var insertNode = require('./insertNode.js');
-var adjustValence = require('./adjustValence.js');
-
+var adjust = require('./adjust.js');
+var leafiness = require( './leafiness.js' );
 
 var Game = function ( ) {
 
 	this.board = {
 
-		'0:0': new Node('A', 0, true),
+		'0:0': new Node('A', 0, false),
 		'0:1': new Node('L', 1),
 		'0:-1': new Node('L', 1),
 		'1:0': new Node('L', 1),
@@ -21,10 +21,12 @@ var Game = function ( ) {
 
 	this.minimumValence = 0;
 
-}
+};
 
 Game.prototype.insert = insertNode;
 
-Game.prototype.adjustValence = adjustValence;
+Game.prototype.adjust = adjust;
+
+Game.prototype.leafiness = leafiness;
 
 module.exports = Game;
