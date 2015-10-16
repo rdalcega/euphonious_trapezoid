10bf0f2e4385c940c0eb55fb9ef7771caf5e42ae
Rodrigo Delgado <rdalcega@gmail.com>
2015-10-16 14:10:38 -0700

    Node insert works.

--- server/game/insertNode.js
+++ server/game/insertNode.js
@@ -1,66 +1,41 @@
-var adjacentKeys = require('./adjacentKeys.js');
+var insertNode = function( coordinates, state ) {
 
-var insertNode = function(coordinates, state) {
+  var node = this.board[ coordinates ];
 
-	if ( !this.board[coordinates] || this.board[coordinates].state !== 'L' ) {
-		// this.board[coordinates] = new Node(state, valence, leafy);
-		return false;
-	} else {
+  if( !node || node.state !== 'L' ) {
 
-    var valence = this.board[coordinates].valence;
+    return false;
 
-    var relativeCoordinates = adjacentKeys(coordinates);
+  } else {
 
-    this.board[coordinates].state = state;
-    this.adjustValence(
-      relativeCoordinates,
-      valence
-    );
+    node.state = state;
 
-  for (var i = 0; i < relativeCoordinates.length; i++) {
-      var sphere = this.board[relativeCoordinates[i]];
-      if (sphere.state !== 'L' ) {
-        if (sphere.valence >= valence) {
-          break;
-        }
-        if (sphere.leafy) {
-          sphere.leafy = !sphere.leafy;
-          this.leaves[sphere.valence] -= 1;
-        } else if (sphere.state === 'A') {
-          this.leaves[0] -= 1;
+    if( node.valence === 1 ) {
 
-          while (this.leaves[this.leaves.length - 1] === 0) {
-            this.leaves.pop();
-          }
-        }
-
-      }
-    }
-
-    if (i === relativeCoordinates.length && this.board[coordinates].leafy === false) {
-      this.board[coordinates].leafy = true;
-      this.leaves[this.board[coordinates].valence] = 
-        this.leaves[this.board[coordinates].valence] || 0;
-
-      this.leaves[this.board[coordinates].valence] += 1;
+      this.leaves[ 0 ] -= 1;
 
     }
 
-    this.maximumValence = Math.max(this.maximumValence, valence);
+    this.adjust( coordinates );
 
-    if (this.leaves[this.minimumValence] === 0) {
-      for (var i = 0; i < this.leaves.length; i++) {
-        if (this.leaves[i] !== 0) { 
-          this.minimumValence = i;
-          break;
-        }
+    this.maximumValence = this.leaves.length - 1;
+
+    for( var i = 0; i < this.leaves.length; i++ ) {
+
+      if( this.leaves[ i ] !== 0 ) {
+
+        this.minimumValence = i;
+
+        break;
+
       }
+
     }
 
     return true;
 
   }
 
-}
+};
 
 module.exports = insertNode;
