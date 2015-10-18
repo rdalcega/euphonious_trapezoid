d9a5efb42de681e6d1f30e4600489f78afc9f21d
Zachary T. Castellanos-Smith <ztsmith25@gmail.com>
2015-10-17 17:07:42 -0700

    add password encryption to authentication

--- server/auth/authCntrl.js
+++ server/auth/authCntrl.js
@@ -1,3 +1,4 @@
+var bcrypt = require('bcrypt-nodejs');
 var db = require('../db/db.js');
 var jwt = require('jsonwebtoken');
 
@@ -6,32 +7,42 @@ module.exports.login = function(req, res) {
   var password = req.body.password; // add decrypt
   //check to see if login credentials are in database (no encrypt yet)
   var sqlQueryAsk = "SELECT Players.Player_Username, Players.Email, \
-    Players.Player_ID FROM Players WHERE \
-    Player_Username = '" + username + "' AND \
-    Password = '" + password + "' LIMIT 1";
+    Players.Player_ID, Players.Hash FROM Players WHERE \
+    Player_Username = '" + username + "' LIMIT 1";
 
   db.query(sqlQueryAsk, function(err, results) {
     if (err) {
       console.log(err);
     } else {
+      var player = results[0];
       if (results.length === 0) {
-        res.status(401).send('Invalid login credentials');
+        res.status(401).send('Invalid Username');
       } else {
-        var profile = { //this is done in callback from db
-          userName: results[0].Player_Username,
-          email: results[0].Email,
-          id: results[0].Player_ID
-        };
+        bcrypt.compare(password, player.Hash, function(err, response) {
+          if (err){
+            console.log(err);
+          } else {
+            if(!response){
+              res.status(401).send('Invalid Password');
+            } else {
+              var profile = { //this is done in callback from db
+                userName: player.Player_Username,
+                email: player.Email,
+                id: player.Player_ID
+              };
 
-        var token = jwt.sign(profile, 'flashing kittens love 25% cotton!', {
-          expiresIn: 60 * 60 * 8
+              var token = jwt.sign(profile, 'flashing kittens love 25% cotton!', {
+                expiresIn: 60 * 60 * 8
+              });
+
+              res.json({
+                profile: profile,
+                token: token
+              });
+            }
+          }
         });
-
-        res.json([profile, {
-          token: token
-        }]);
       }
     }
   });
-
 };
--- server/player/playerCntrl.js
+++ server/player/playerCntrl.js
@@ -1,3 +1,4 @@
+var bcrypt = require('bcrypt-nodejs');
 var db = require('../db/db.js');
 
 module.exports.signup = function(req, res) {
@@ -8,7 +9,7 @@ module.exports.signup = function(req, res) {
   var sqlQueryAsk = "SELECT Players.Player_Username FROM \
     Players WHERE Player_Username = '" + username + "' \
     LIMIT 1";
-  var sqlQueryIns = "INSERT INTO Players (Player_Username, Password, Email) \
+  var sqlQueryIns = "INSERT INTO Players (Player_Username, Hash, Email) \
     Value (?, ?, ?)";
 
   db.query(sqlQueryAsk, function(err, results) {
@@ -19,11 +20,19 @@ module.exports.signup = function(req, res) {
       if (results.length !== 0) { // if entry exists then kick back and ask for new input
         res.status(403).send('username already taken');
       } else { // if entry does not exist then submit into to database kick to login on the client
-        db.query(sqlQueryIns, params, function(err) {
-          if (err) {
+        bcrypt.hash(password, null, null, function(err, hash) {
+          if(err){
             console.log(err);
           } else {
-            res.status(200).send('credentials accepted, redirect to login');
+            console.log(hash)
+            params[1] = hash;
+            db.query(sqlQueryIns, params, function(err) {
+              if (err) {
+                console.log(err);
+              } else {
+                res.status(200).send('credentials accepted, redirect to login');
+              }
+            });
           }
         });
       }
