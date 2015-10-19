bc3a368293535f88171720b163633559adb61e57
Zachary T. Castellanos-Smith <ztsmith25@gmail.com>
2015-10-19 15:25:53 -0700

    add nav, launch, and routes

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,4 +1,4 @@
-var gulp = require('gulp');
+gvar gulp = require('gulp');
 var gutil = require('gulp-util');
 var bower = require('bower');
 var concat = require('gulp-concat');
---
+++ client/www/launch/launchController.js
@@ -0,0 +1,5 @@
+sphero.controller('launchController', [ function() {
+
+
+
+}]);
