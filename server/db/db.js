f015e368d7072426df19d623c307ffffd5482f2c
Zachary T. Castellanos-Smith <ztsmith25@gmail.com>
2015-10-20 12:23:55 -0700

    add connection to cleardb

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var pool = mysql.createPool({
-  user: 'root',
-  password: '',
-  database: 'Sphero',
-  host: 'localhost'
+  user: 'bb13d03ae42809',
+  password: 'f5675c11',
+  database: 'heroku_a35b008f64f7309',
+  host: 'us-cdbr-iron-east-03.cleardb.net'
 });
 
 // export the db connection
