418e34eed5ce278001af2b31b19be5eb19b79fcf
jolthoff <jl.olthoff@gmail.com>
2015-10-21 12:26:11 -0700

    TAFT

--- client/www/launch/launchController.js
+++ client/www/launch/launchController.js
@@ -1,13 +1,13 @@
 sphero.controller('launchController', ['$scope', 'socket', function($scope, socket) {
 
 	$scope.join = function() {
-    console.log('joined');
+    	console.log('joined');
 		socket.emit('join');
 
 	}
 
 	$scope.host = function() {
-    console.log('hosting');
+    	console.log('hosting');
 		socket.emit('host');
 
 	}
