3ddfff7a0dcc2f5ef96e2735e5c08a1bdfa9200e
jolthoff <jl.olthoff@gmail.com>
2015-10-21 11:19:42 -0700

    Add host and join functions in launch controller

--- client/www/launch/launchController.js
+++ client/www/launch/launchController.js
@@ -1,5 +1,15 @@
-sphero.controller('launchController', [ function() {
+sphero.controller('launchController', ['$scope', 'socket', function($scope, socket) {
 
+	$scope.join = function() {
+    console.log('joined');
+		socket.emit('join');
 
+	}
+
+	$scope.host = function() {
+    console.log('hosting');
+		socket.emit('host');
+
+	}
 
 }]);
