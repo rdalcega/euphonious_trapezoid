d0e05943cfd729c6085a024aaaa3dea48c0a932b
Zachary T. Castellanos-Smith <ztsmith25@gmail.com>
2015-10-20 19:09:51 -0700

    change app to server

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 
 var port = process.env.PORT || 8080;
-app.listen(port);
+server.listen(port);
 
 //pass app and express to middleware
 require('./config/middleware.js')(app, express);
