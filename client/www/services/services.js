aebbb8537193aad3e33963a037e4dc73a053b7d0
jolthoff <jl.olthoff@gmail.com>
2015-10-21 17:22:03 -0700

    TAFT

--- client/www/services/services.js
+++ client/www/services/services.js
@@ -41,13 +41,14 @@ sphero.factory('Auth', ['$http', 'SpheroApiUrl', function($http, SpheroApiUrl) {
 
 /* This factory sets up a socket connection and gives you .on and .emit methods to use.
 */
-sphero.factory('socket', ['SpheroApiUrl', function (SpheroApiUrl) {
+sphero.factory('socket', ['SpheroApiUrl', '$rootScope', function (SpheroApiUrl, $rootScope) {
    var socket;
    // if (window.__karma__) { //in case we do testing with karma and phantom
    //   socket = io.connect(window.location.protocol + "//" + window.location.hostname + ":" + 1337);
    // } else {
      socket = io.connect(SpheroApiUrl);
    // }
+
     socket.on('connect', function() {
 
       socket.on('started', function(data) {
@@ -60,15 +61,22 @@ sphero.factory('socket', ['SpheroApiUrl', function (SpheroApiUrl) {
 
   return {
     on: function (eventName, callback) {
-      socket.on(eventName, callback);
+      socket.on(eventName, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
     },
     emit: function (eventName, data, callback) {
       console.log('emitting...')
       socket.emit(eventName, data, function () {
         var args = arguments;
-        if (callback) {
-          callback.apply(socket, args);
-        }
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
       });
     }
   };
--- server/config/socketconnection.js
+++ server/config/socketconnection.js
@@ -10,16 +10,7 @@ module.exports = function(io) {
 
   io.on('connection', function(socket) {
     console.log('a user connected');
-    // socket.emit('started');
-    // io.emit('started', null, function() {
-    //   console.log("I've emitted started");
-    // });
-  socket.emit('started', function() {
-    console.log("I HEARD STARTED");
-  });
-  socket.on('started', function() {
-    console.log("CLIENT EMITTED STARTED");
-  })
+
     Sphero.init(io, socket);
     socket.on('disconnect', function(){
       console.log('a user disconnected');
