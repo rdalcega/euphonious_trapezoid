10d541be4a8d954d0efb21df078f08a664511a11
xerxesv <xerxesv@gmail.com>
2015-10-22 04:35:18 +0100

    MVP dude

--- client/www/game/gameController.js
+++ client/www/game/gameController.js
@@ -6,37 +6,40 @@ sphero.controller('gameController', ['game', 'socket', 'player', function(game, 
 
   game.init(element, 2000, 20);
 
-  console.log("game.playerNum: ", game.playerNum);
+  console.log("game.playerNum: ", player.playerNum);
   window.addEventListener('resize', function() {
     game.resize();
   });
 
-  // game.addPiece({coordinates: {x: 3, y: 2}, state: game.playerNum});
-  // game.addPiece({coordinates: {x: 0, y: -1}, state: game.playerNum});
+  // game.addPiece({coordinates: {x: 3, y: 2}, state: player.playerNum});
+  // game.addPiece({coordinates: {x: 0, y: -1}, state: player.playerNum});
 
 
   window.addEventListener('mousedown', function (mouseDownEvent) {
     var pos = game.getGridPosition(mouseDownEvent, function (position2d) {
-      socket.emit('insert', {coordinates: position2d, state:game.playerNum }, function () {
-        console.log('emitted: ', {coordinates: position2d, state:game.playerNum });
+
+      socket.emit('insert', {coordinates: position2d, state:player.playerNum }, function () {
+        console.log('emitted: ', {coordinates: position2d, state:player.playerNum });
       });
     });
 
+    // game.removePiece({coordinates: {x: 0, y: 0}, state:'A', success:true})
     //testing
-    // console.log("pos, game.playerNum: ", pos, game.playerNum);
+    // console.log("pos, player.playerNum: ", pos, player.playerNum);
     // game.rotateBoard([
-    //   {from: {x: 3, y: 2}, to: {x: 2, y:-3}, state: game.playerNum, success:true},
-    //   {from: {x: 0, y: -1}, to: {x: -1, y:0}, state: game.playerNum, success:true}
+    //   {from: {x: 3, y: 2}, to: {x: 2, y:-3}, state: player.playerNum, success:true},
+    //   {from: {x: 0, y: -1}, to: {x: -1, y:0}, state: player.playerNum, success:true}
     // ]);
 //    game.movePiece({from: {x: pos.x, y: pos.y}, to: {x: 1, y:1}, state: game.playerNum, success:true});
 
   }, false);
 
   socket.on('put', function (data) {
+    console.log('got some put data: ', data);    
     if (data.success) {
       game.addPiece(data);
     } else {
-      console.log('put data.error === true');
+      console.log('data.success === false');
     }
   });
 
