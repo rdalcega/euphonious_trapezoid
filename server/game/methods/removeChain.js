6444d060205328071579483e1b8b3aabf36ae68e
jolthoff <jl.olthoff@gmail.com>
2015-10-22 19:01:43 -0700

    Add check for piece on board before sending remove event to client

--- server/game/methods/removeChain.js
+++ server/game/methods/removeChain.js
@@ -58,7 +58,7 @@ var removeChain = function( chain ) {
     }
     // At this point the piece has been
     // removed.
-    event.success = true;
+    event.success = true;s
     this.emit( 'removed', event );
     // Reanchor is the function that drags
     // the possibly unanchored pieces into
@@ -77,8 +77,12 @@ var removeChain = function( chain ) {
   // Removing the chain consists in
   // removing each sphere that makes up the
   // chain.
+  var state = this.get(chain[0].x, chain[0].y).state
   chain.forEach( function( coordinates ) {
-    remove( coordinates.x, coordinates.y );
+    var sphere = this.get(coordinates.x, coordinates.y);
+    if (sphere && sphere.state === state) {
+      remove( coordinates.x, coordinates.y );
+    }
   }.bind( this ));
 };
 module.exports = removeChain;
