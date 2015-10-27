6971fb179e2c989654f7982612b2447889b8a914
jolthoff <jl.olthoff@gmail.com>
2015-10-27 14:29:27 -0700

    Remove localhost connection

--- server/db/db.js
+++ server/db/db.js
@@ -7,12 +7,5 @@ var pool = mysql.createPool({
   host: 'us-cdbr-iron-east-03.cleardb.net'
 });
 
-// var pool = mysql.createPool({
-// 	user: 'root',
-// 	password: '',
-// 	database: 'heroku_a35b008f64f7309',
-// 	host: 'localhost'
-//  });
-
 // export the db connection
 module.exports = pool;
