82e0660fc30bf3cc2958168f027832ea7d4b7e4d
jolthoff <jl.olthoff@gmail.com>
2015-10-28 16:14:02 -0700

    Remove connection to insert method on game when game ends

--- client/www/game/gameController.js
+++ client/www/game/gameController.js
@@ -8,6 +8,7 @@ sphero.controller('gameController', ['$scope', '$state', 'game', 'socket', 'play
   console.log('game.playerNum: ', game.playerNum);
 
   game.init(element, 20);
+  var gameEnded = false;
 
   var eventQueue = [];
   setInterval(function() {
@@ -32,6 +33,7 @@ sphero.controller('gameController', ['$scope', '$state', 'game', 'socket', 'play
       state: game.playerNum
     };
     console.log(sending);
+
     if (!gameEnded) {
       socket.emit('insert', {
         coordinates: coordinates,
