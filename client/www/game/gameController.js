0d98b011af9a8cee04b64e6c1848e57910edb35d
Zachary T. Castellanos-Smith <ztsmith25@gmail.com>
2015-10-28 19:59:22 -0700

    update endgame modal and html template to pull actual player array from server

--- client/www/game/gameController.js
+++ client/www/game/gameController.js
@@ -48,11 +48,22 @@ sphero.controller('gameController', ['$scope', '$state', 'game', 'socket', 'play
     });
   });
 
-  $scope.showPopup = function(playersObj) {
-    $scope.endGame = playersObj; // look at what this obj is and extract
-    console.log(playersObj); 
+  $scope.showPopup = function(playersArray) {
+    $scope.endGame = []; // look at what this obj is and extract
+    console.log('in popup ==============', playersArray); 
+    $scope.me = null;
+    $scope.place = null;
+    $scope.placeObj = {'1': '1st', '2': '2nd', '3': '3rd', '4': '4th'};
     // an array with players profiles in order of their rank for current game
-    
+    for(var i = 0; i < playersArray.length; i++){
+      if(playersArray[i]){
+        $scope.endGame.push(playersArray[i]);
+        if(playersArray[i].username === player.profile.username){
+          me = playersArray[i];
+          place = i;
+        }
+      }
+    }
     //allow player to friend other players
     $scope.friend = function(otherPlayer) {
       Auth.addFriend(otherPlayer, player.profile.id);
