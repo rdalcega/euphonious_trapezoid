a0587e585ad542925983475f592bcac0f1d0ce16
Rodrigo Delgado <rdalcega@gmail.com>
2015-10-30 12:30:08 -0700

    Create first draft of SequenceElements

--- client/www/music/SequenceElement.js
+++ client/www/music/SequenceElement.js
@@ -1,7 +1,10 @@
-window.AudioContext.prototype.createSequenceElement = function( midiNote, velocity ) {
+window.AudioContext.prototype.createSequenceElement = function( midiNote, valence ) {
   /*
     GRAPH:
     element.sub --> *
+                    |
+                    V
+             element.sub.gain
                     |
                     v
     element.carrier * --> element.carrier.gain --> element.master --> destination
@@ -24,9 +27,134 @@ window.AudioContext.prototype.createSequenceElement = function( midiNote, veloci
   var context = this;
   // midiNote is randomized very slightly
   midiNote = midiNote + Math.random() * 0.1 - 0.05;
-  // velocity is randomized very slightly
-  velocity = velocity + Math.random( ) * 5 - 2.5;
-  if( velocity < 0 ) {
-    velocity = 0;
-  }
+  var element = context.createSynthesizer();
+  var ms = Math.pow( 10, -3 );
+  // The elements sustain time depends on the
+  // element's valence
+  var sustain = valence * 100 + Math.random() * 100; // ms
+  element.setSustain( sustain * ms );
+  element.setMasterGain( 1 );
+  // Create and configure element.carrier
+  element.carrier = context.createOscillator();
+  element.carrier.type = 'sine';
+  element.carrier.frequency.value = 3 * 440 * Math.pow( 2, ( midiNote - 69 ) / 12 );
+  element.carrier.start( context.currentTime );
+  // Create and configure the element.carrier.gain
+  element.carrier.gain = context.createGain( );
+  element.carrier.gain.gain.value = 0;
+  /*
+    element.carrier --> element.carrier.gain
+  */
+  element.carrier.connect( element.carrier.gain );
+  // Create and configure element.sub
+  element.sub = context.createOscillator();
+  element.carrier.type = 'sine';
+  element.carrier.frequency.value = 0.5 * 440 * Math.pow( 2, ( midiNote - 69 ) / 12 );
+  element.carrier.start( context.currentTime );
+  // Create and configure the element.sub.gain
+  element.sub.gain = context.createGain( );
+  element.sub.gain = 1;
+  /*
+    element.sub --> element.sub.gain
+  */
+  element.sub.connect( element.sub.gain );
+  /*
+    element.sub.gain --> element.carrier.gain
+  */
+  element.sub.gain.connect( element.carrier.gain );
+  /*
+    element.carrier.gain --> element.master
+  */
+  element.carrier.gain.connect( element.master.input );
+  // Create and configure element.carrier.gain.envelope
+  element.carrier.gain.envelope = {};
+  // Attack time is proportional to valence
+  // Attack target is inversely proptional to valence
+  element.carrier.gain.envelope.attack = {
+    time: valence * 10 + Math.random( ) * 10,
+    target: 1 / valence,
+    initial: 0
+  };
+  // Decay time is proportional to valence
+  element.carrier.gain.envelope.decay = 10 + ( valence - 1 ) * 30 + Math.random( ) * 30;
+  // Sustain target is inversely proportional to valence
+  element.carrier.gain.envelope.sustain = 0.5 / valence;
+  // Release target is always 0
+  // Release time is directly proportional to valence
+  element.carrier.gain.envelope.release = {
+    time: valence * 25 + Math.random( ) * 25,
+    target: 0
+  };
+  element.carrier.gain.envelope = context.createEnvelope(
+    element.carrier.gain.envelope.attack,
+    element.carrier.gain.envelope.decay,
+    element.carrier.gain.envelope.sustain,
+    element.carrier.gain.envelope.release
+  );
+  element.envelopes.push( element.carrier.gain.envelope );
+  /*
+    element.carrier.gain
+                   ^
+                   |
+                   .gain
+                   ^
+                   |
+                   * <-- element.carrier.gain.envelope
+  */
+  element.carrier.gain.envelope.connect( element.carrier.gain.gain );
+  // Create and configure element.carrier.modulator
+  element.carrier.modulator = context.createOscillator();
+  element.carrier.modulator.type = 'sine';
+  element.carrier.modulator.frequency.value = 440 * Math.pow( 2, ( midiNote - 69 ) / 12 );
+  element.carrier.modulator.frequency.detune = -50;
+  element.carrier.modulator.start( context.currentTime );
+  // Create and configure element.carrier.modulator.gain
+  element.carrier.modulator.gain = context.createGain( );
+  element.carrier.modulator.gain.gain.value = 5 + Math.random( ) * 5;
+  /*
+    element.carrier
+           ^
+           |
+           .frequency
+           ^
+           |
+           * <-- element.carrier.modulator.gain
+  */
+  element.carrier.modulator.gain.connect( element.carrier.frequency );
+  /*
+    element.carrier.modulator.gain <-- element.carrier.modulator
+  */
+  element.carrier.modulator.connect( element.carrier.modulator.gain );
+  // Create and congifure element.carrier.modulator.gain.envelope
+  element.carrier.modulator.gain.envelope = {};
+  // The attack target is directly proportional to the valence
+  element.carrier.modulator.gain.envelope.attack = {
+    target: element.carrier.modulator.gain.gain.value * valence,
+    time: element.carrier.gain.envelope.attack.time + element.carrier.gain.envelope.decay.time,
+    initial: element.carrier.modulator.gain.gain.value
+  };
+  element.carrier.modulator.gain.envelope.decay = 0;
+
+  element.carrier.modulator.gain.envelope.sustain = element.carrier.modulator.gain.envelope.attack.target;
+  element.carrier.modulator.gain.envelope.release = {
+    time: element.carrier.gain.envelope.release.time,
+    target: element.carrier.modulator.gain.gain.value
+  };
+  element.carrier.modulator.gain.envelope = context.createEnvelope(
+    element.carrier.modulator.gain.envelope.attack,
+    element.carrier.modulator.gain.envelope.decay,
+    element.carrier.modulator.gain.envelope.sustain,
+    element.carrier.modulator.gain.envelope.release
+  );
+  element.envelopes.push( element.carrier.modulator.gain.envelope );
+  /*
+    element.carrier.modulator.gain
+                             ^
+                             |
+                             .gain
+                             ^
+                             |
+                             * <-- element.carrier.modulator.gain.envelope
+  */
+  element.carrier.modulator.gain.envelope.connect( element.carrier.modulator.gain );
 };
