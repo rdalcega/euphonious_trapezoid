ceda3d22759f859b143db4ff6b7b4d965063e7e9
jolthoff <jl.olthoff@gmail.com>
2015-10-30 10:35:48 -0700

    Remove shutting off insert event on client

--- client/www/game/gameController.js
+++ client/www/game/gameController.js
@@ -4,7 +4,7 @@ sphero.controller('gameController', ['$scope', '$state', 'game', 'socket', 'play
   element = document.getElementById("game");
 
   var gameEnded = false;
-  var lastTimePlayed = Date.now();
+  // var lastTimePlayed = Date.now();
 
   game.playerNum = String(player.playerNum);
 
@@ -49,13 +49,13 @@ sphero.controller('gameController', ['$scope', '$state', 'game', 'socket', 'play
     var coordinates = game.getPosition( mouseDownEvent.clientX, mouseDownEvent.clientY );
     var sending = {coordinates: coordinates, state: game.playerNum };
 
-    if (!gameEnded && Date.now() > lastTimePlayed - 500) {
-      lastTimePlayed = Date.now();
+    // if (!gameEnded && Date.now() > lastTimePlayed - 500) {
+    //   lastTimePlayed = Date.now();
       socket.emit('insert', {
         coordinates: coordinates,
         state: game.playerNum
       });
-    }
+    // }
   }, false);
 
 // listener for testing solo games, remove in production
