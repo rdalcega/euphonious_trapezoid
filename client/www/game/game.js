3c728eb134ea3222a55f86f3d9e1215b2f2655f0
xerxesv <xerxesv@gmail.com>
2015-10-31 01:22:54 +0100

    implement showing whose turn it is on client

--- client/www/game/game.js
+++ client/www/game/game.js
@@ -2,6 +2,7 @@ sphero.factory('game', function () {
 
   var gameDomElement;
   var svg;
+  var background;
   var grid;
 
   var gameWidth;
@@ -11,13 +12,16 @@ sphero.factory('game', function () {
   var gridStepIncrement;
   var wiggleRoom;
 
+
   var playerNum;
+  var playersTurn;
+
   var colors = { 
-  A: ["#fd3132", "#8a2225"], 
-  0: ["#fc9bcb", "#fccfe6"], // light pink #fccfe6 dark pink #b96890
-  1: ["#97d9a1", "#c3d9c6"], // light green #c3d9c6
-  2: ["#00a8db", "#017ba0"], // light blue #6ec2db
-  3: ["#787b8c", "#b2b5c3"] }; // light gray #b2b5c3 dark gray #525460
+  A: ["#fd3132", "#8a2225", "#ffffff"], 
+  0: ["#fc9bcb", "#fccfe6", "#8c3e65"], // light pink #fccfe6 dark pink #8c3e65 other dark pink #b96890
+  1: ["#97d9a1", "#c3d9c6", "#499755"], // light green #c3d9c6 dark green #499755
+  2: ["#00a8db", "#017ba0", "#017ba0"], // light blue #6ec2db dark blue #017ba0
+  3: ["#787b8c", "#b2b5c3", "#525460"] }; // light gray #b2b5c3 dark gray #525460
 
   var radius;
 
@@ -42,6 +46,24 @@ sphero.factory('game', function () {
 
   };
 
+  var showTurnChange = function (player, duration) {
+
+    d3.select(".A")
+      .attr('cx', '50%')
+      .attr('cy', '50%')
+      .style('fill', colors[player][2])
+      .attr('r', Number(radius.slice(0, -1)) * .2 + "%")
+
+      .transition()
+      .duration(duration/2) // change 1000 to a variable representing turn duration
+      .attr('r', Number(radius.slice(0, -1)) * (2+wiggleRoom)/2 + "%" )
+
+      .transition()
+      .duration(duration/2)
+      .attr('r', Number(radius.slice(0, -1)) * .2 + "%" );
+
+  };
+
   var updateBoard = function ( data ) {
     console.log("data: ", data );
     var duration = 50;
@@ -53,7 +75,10 @@ sphero.factory('game', function () {
     spheres.enter()
       .append('circle')
       .attr("r", 0)
-      .attr("fill", "none");
+      .attr("fill", "none")
+      .attr("class", function (d) {
+        return d.state;
+      });
 
     spheres
       .transition()
@@ -68,9 +93,17 @@ sphero.factory('game', function () {
         var posString = String(pos) + "%";
         return posString;
       })
-      .attr("r", radius)
+      .attr("r", function (d) {
+        if (d.state !== 'A') {
+          return radius;
+        } else {
+//          return Number(radius.slice(0, -1)) * .2 + "%";
+        }
+      })
       .style("fill", function (d) {
-        return colors[d.state][0];
+        if (d.state !== 'A') {
+          return colors[d.state][0];
+        } 
       });
 
     spheres.exit().remove();
@@ -118,6 +151,7 @@ sphero.factory('game', function () {
       d3.select("#grid").append("circle").datum( {coordinates: data.coordinates, id: data.id, state: data.state} )
 
       .attr("r", 0)
+      .attr("class", function (d) { return d.state; })
       .style("fill", "white")
       .attr("cx", getSvgPosition(data.coordinates).x)
       .attr("cy", getSvgPosition(data.coordinates).y)
@@ -391,6 +425,7 @@ sphero.factory('game', function () {
 
 
   var init = function (element, size) {
+    console.log('playerNum in init', playerNum);
     gameDomElement = element || document.getElementById("game");
     gridSize = size || 20;
     wiggleRoom = wiggleRoom || .5;
@@ -406,7 +441,7 @@ sphero.factory('game', function () {
     gradient.append("stop").attr("offset", "0%").attr("stop-color", "#010708");
     gradient.append("stop").attr("offset", "70%").attr("stop-color", "#011218");
 
-    svg.append("rect").attr("width", "100%").attr("height", "100%")
+    background = svg.append("rect").attr("width", "100%").attr("height", "100%")
     .attr("id", "gradientBackground").attr("fill", "url(#gameGradient)");
 
     grid = svg.append("svg").attr("id", "grid");
@@ -427,9 +462,12 @@ sphero.factory('game', function () {
   return {
 
     playerNum: playerNum,
+    playersTurn: playersTurn,
+    colors: colors,
 
     init: init,
     setSize: setSize,
+    showTurnChange: showTurnChange,
     getPosition: getPosition,
     updateBoard: updateBoard,
     put: put,
