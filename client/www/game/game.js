4e96d991c9149a0ca8251ff23b15e37cf4ccb5c7
Rodrigo Delgado <rdalcega@gmail.com>
2015-11-03 11:43:24 -0800

    Back to working state without music

--- client/www/game/game.js
+++ client/www/game/game.js
@@ -1,14 +1,4 @@
 sphero.factory('game', ['scales', function (scales) {
-  // MUSIC STUFFS!
-  // ------
-  var context = new AudioContext( );
-  var elements = {}; // { id: { element: sequenceElement, note: integer } }
-  var scale; // The working scale is not chosen until the first element is placed on the board
-  var filter = context.createBiquadFilter();
-  filter.type = 'lowpass';
-  filter.frequency.value = 1000;
-  filter.connect( context.destination );
-  // -------
   var gameDomElement;
   var svg;
   var background;
@@ -119,39 +109,6 @@ sphero.factory('game', ['scales', function (scales) {
       });
 
     spheres.exit().remove();
-    // -----
-    // Update the filter frequency.
-    // The more spheres there are on the
-    // game, the larger the filter frequency.
-    // Note that a board with valence 8 can have at most
-    // 113 spheres on a terminating condition
-    filter.frequency.value = 500 + data.length * 2500 / 113;
-    // Choose scale if non exists or if the number of pieces
-    // in the board is less than the scale's length
-    // if( data.length < scale.length ) {
-    //   // We use a random index offset to ensure we choose
-    //   // a new scale each time around.
-    //   var offset = Math.floor( Math.random( ) * ( scales.length - 1 ) );
-    //   // The new scale must contain the note corresponding
-    //   // to each sphere on the board.
-    //   for( var i = 0; i < scales.length; i++ ) {
-    //     var aScale = scales[ ( i + offset ) % scales.length ];
-    //     var valid = true;
-    //     for( var j = 0; j < data.length; j++ ) {
-    //       var sphere = data[ j ];
-
-    //       if( aScale.indexOf( sphere.note % 12 ) < 0 ) {
-    //         valid = false;
-    //         break;
-    //       }
-    //     }
-    //     if( valid ) {
-    //       scale = aScale;
-    //       break;
-    //     }
-    //   }
-    // }
-    // ----
     return duration;
   };
 
@@ -192,97 +149,6 @@ sphero.factory('game', ['scales', function (scales) {
   var put = function (data) {
     var duration;
     if (data.success) {
-      // -----
-      // We create a new sequence element that will correspond to this
-      // sphere until it's valence changes.
-      // We choose the midiNote
-      var midiNote;
-      // In order to choose the note,
-      // we will need access to the sphere's
-      // neighbors.
-      var neighbors = d3.select('#grid').selectAll('circle').filter( function(d) {
-        var condition =
-          d.coordinates.x === data.coordinates.x + 1 && d.coordinates.y === data.coordinates.y ||
-          d.coordinates.x === data.coordinates.x - 1 && d.coordinates.y === data.coordinates.y ||
-          d.coordinates.x === data.coordinates.x && d.coordinates.y === data.coordinates.y + 1 ||
-          d.coordinates.x === data.coordinates.x && d.coordinates.y === data.coordinates.y - 1;
-        return condition;
-      });
-      // In the special case that the only neighbor
-      // of a sphere is the anchor, the note for
-      // the element is chosen randomly within the chosen
-      // scale and within the 24 - 36 octave.
-      if( data.valence === 1 && neighbors.length === 1 ) {
-        midiNote = 36 + scale[ ~~( Math.random( ) * scale.length ) ];
-      } else {
-        // In every other case, the value of the midiNote
-        // is determined from one of it's neighbors. Preference
-        // is given to notes on the scale that are closest to the
-        // note of one of it's neighbors.
-        var absNeighborMidiNote = 0;
-        neighbors.each( function( d ) {
-          if( elements[ d.id ] ) {
-            absNeighborMidiNote += elements[d.id].midiNote;
-          }
-        });
-        absNeighborMidiNote = Math.floor( absNeighborMidiNote / neighbors.size() );
-        console.log( 'average neighbor: ', absNeighborMidiNote );
-        var relNeighborMidiNote = absNeighborMidiNote % 12;
-        var octave = absNeighborMidiNote - relNeighborMidiNote;
-        var screen = Math.random( );
-        var stepSize;
-        if( screen < 0.25 ) {
-          stepSize = ( ~~(Math.random( ) * 2)*2 - 1 ) * 4; // third
-        } else if( screen < 0.5 ) {
-          stepSize = ( ~~(Math.random( ) * 2)*2 - 1 ) * 5; // fourth
-        } else if( screen < 0.70 ) {
-          stepSize = ( ~~(Math.random( ) * 2)*2 - 1 ) * 7; // fifth
-        } else if( screen < 0.85 ) {
-          stepSize = ( ~~(Math.random( ) * 2)*2 - 1 ) * 2; // whole
-        } else if( screen < 0.925 ) {
-          stepSize = ( ~~(Math.random( ) * 2)*2 - 1 ) * 9; // sixth
-        } else if( screen < 0.975 ) {
-          stepSize = ( ~~(Math.random( ) * 2)*2 - 1 ) * 11; // seventh
-        } else {
-          stepSize = ( ~~(Math.random( ) * 2)*2 - 1 ) * 12; //octave
-        }
-        midiNote = relNeighborMidiNote + stepSize;
-        if( midiNote > 12 ) {
-          octave += 12;
-          midiNote = midiNote % 12;
-        } else if( midiNote < 0 ) {
-          octave -= 12;
-          midiNote = midiNote + 12;
-        }
-        midiNote = scale.reduce( function( closest, current ) {
-          if( Math.abs( midiNote - closest ) > Math.abs( midiNote - current ) ) {
-            return current;
-          } else {
-            return closest;
-          }
-        });
-        midiNote += octave;
-        if( midiNote < 36 ) {
-          midiNote = 36 + scale[ 0 ];
-        } else if( midiNote > 84 ) {
-          midiNote = 72 + scale[ scale.length - 1 ];
-        }
-      }
-      // Now that we have the midiNote, we can create the remove sound
-      var removed = context.createRemovedElement( midiNote, data.valence );
-      //var put = context.createPutElement( midiNote, data.valence );
-      var sequence = context.createSequenceElement( midiNote, data.valence );
-      removed.connect( filter );
-      sequence.connect( filter );
-      //put.connect( filter );
-      //put.start( context.currentTime );
-      elements[ data.id ] = {
-        removed: removed,
-        //put: put,
-        sequence: sequence,
-        midiNote: midiNote
-      };
-      // -----
       duration = 100;
       d3.select("#grid").append("circle").datum( {coordinates: data.coordinates, id: data.id, state: data.state} )
       .attr("r", 0)
@@ -366,18 +232,10 @@ sphero.factory('game', ['scales', function (scales) {
     .attr("r", Number(radius.slice(0, -1)) * (2+wiggleRoom)/2 + "%") 
 //    .style("stroke-width", 1)
     .style("fill", colors[data.state][1])
-    .each( 'start', function( d ) {
-      elements[ d.id ].removed.start( context.currentTime );
-    })
     .transition()
     .duration( duration/3 )
     .ease("linear")
     .attr("r", 0)
-    .each('end', function( d ) {
-      elements[ d.id ].removed.disconnect( );
-      //elements[ d.id ].put.disconnect( );
-      delete elements[ d.id ];
-    })
     .remove();
 
     return duration + 25;
@@ -716,128 +574,128 @@ sphero.factory('game', ['scales', function (scales) {
     //     .attr("r", '2%')
     //     .style("fill", "blue")
     // ---
-    // MUSIC stuff
-    if( !scale ) {
-      scale = scales[ Math.floor( Math.random() * ( scales.length - 1 ))];
-    }
-    // Create the anchor element
-    var anchorElement = context.createAnchorElement( 24 + scale[ 0 ] );
-    anchorElement.connect( filter );
-    // Sequence
-    var ms = Math.pow( 10, -3 );
-    var timeoutLength = 300 * ms; // ms
-    var sixteenthTime = 125 * ms; // ms
-    var lastScheduledTime = context.currentTime;
-    var lastScheduledAnchorTime = context.currentTime;
-    var lastScheduledCoordinates;
-    var sequence = function( ) {
-      setTimeout( sequence, timeoutLength );
-      var timeoutStart = context.currentTime;
-      var screen = Math.random( );
-      while( lastScheduledTime + sixteenthTime < timeoutStart + timeoutLength ) {
-        if( lastScheduledAnchorTime + 8 * sixteenthTime < timeoutStart + timeoutLength ) {
-          anchorElement.start( lastScheduledAnchorTime + 8 * sixteenthTime );
-          lastScheduledAnchorTime += 8 * sixteenthTime;
-          d3.select('#grid').selectAll('circle').filter( function( d ) {
-            return d.state === 'A';
-          }).transition( )
-            .delay( ( lastScheduledAnchorTime - context.currentTime ) * 1000 )
-            .duration( 1 * sixteenthTime * 1000 )
-            .ease( 'cubic-in-out' )
-            .attr('r', Number( radius.slice( 0, -1 ) )*0.75 + '%' )
-            .style( 'fill', function( d ) {
-              return colors[ d.state ][ 1 ];
-            })
-            .transition( )
-            .duration( 3 * sixteenthTime * 1000 )
-            .ease( 'bounce' )
-            .attr('r', radius)
-            .style( 'fill', function( d ) {
-              return colors[ d.state ][ 0 ];
-            });
-        }
-        var ids = Object.keys( elements );
-        if( ids.length > 0 && screen > 0.1 ) {
-          if( !lastScheduledCoordinates ) {
-            var choice = ~~( Math.random( ) * ids.length );
-            var sphere = d3.select('#grid').selectAll('circle')
-              .filter( function( d ) {
-                return Number( ids[ choice ] ) === d.id;
-              });
-            sphere.each( function( d ) {
-              lastScheduledCoordinates = d.coordinates;
-            });
-            elements[ ids[ choice ] ].sequence.start( lastScheduledTime + sixteenthTime );
-            lastScheduledTime += sixteenthTime;
-            sphere.transition( )
-              .delay( ( lastScheduledTime - context.currentTime ) * 1000 )
-              .duration( ( sixteenthTime / 2 ) * 1000 )
-              .ease( 'cubic-in-out' )
-              .attr( 'r', Number( radius.slice( 0, -1 ) ) * 0.75 + '%' )
-              .style( 'fill', function( d ) {
-                return colors[ d.state ][ 1 ];
-              })
-              .transition( )
-              .duration( ( sixteenthTime / 2 ) * 1000 )
-              .ease( 'elastic' )
-              .attr( 'r', radius )
-              .style( 'fill', function( d ) {
-                return colors[ d.state ][ 0 ];
-              });
-          } else {
-            var neighbors = d3.select('#grid').selectAll('circle').filter( function(d) {
-              var condition =
-                d.coordinates.x === lastScheduledCoordinates.x + 1 && d.coordinates.y === lastScheduledCoordinates.y ||
-                d.coordinates.x === lastScheduledCoordinates.x - 1 && d.coordinates.y === lastScheduledCoordinates.y ||
-                d.coordinates.x === lastScheduledCoordinates.x && d.coordinates.y === lastScheduledCoordinates.y + 1 ||
-                d.coordinates.x === lastScheduledCoordinates.x && d.coordinates.y === lastScheduledCoordinates.y - 1;
-              return condition;
-            });
-            var choice = ~~(Math.random()*neighbors.size( ));
-            var id;
-            neighbors.each( function( d, i ) {
-              if( i === choice && d.state !== 'A' ) {
-                id = d.id;
-              } else if( i === choice ) {
-                id = -1;
-              }
-            });
-            if( id >= 0 ) {
-              var sphere = d3.select('#grid').selectAll('circle')
-                .filter( function( d ) {
-                  return id === d.id;
-                });
-              sphere.each( function( d ) {
-                lastScheduledCoordinates = d.coordinates;
-              });
-              elements[ id ].sequence.start( lastScheduledTime + sixteenthTime );
-              lastScheduledTime += sixteenthTime;
-              sphere.transition( )
-                .delay( ( lastScheduledTime - context.currentTime ) * 1000 )
-                .duration( ( sixteenthTime / 2 ) * 1000 )
-                .ease( 'cubic-in-out' )
-                .attr( 'r', Number( radius.slice( 0, -1 ) ) * 0.75 + '%' )
-                .style( 'fill', function( d ) {
-                  return colors[ d.state ][ 1 ];
-                })
-                .transition( )
-                .duration( ( sixteenthTime / 2 ) * 1000 )
-                .ease( 'elastic' )
-                .attr( 'r', radius )
-                .style( 'fill', function( d ) {
-                  return colors[ d.state ][ 0 ];
-                });
-            } else {
-              lastScheduledCoordinates = undefined;
-              lastScheduledTime += sixteenthTime;
-            }
-          }
-        } else {
-          lastScheduledTime += sixteenthTime;
-        }
-      }
-    };
-    sequence( );
+    // // MUSIC stuff
+    // if( !scale ) {
+    //   scale = scales[ Math.floor( Math.random() * ( scales.length - 1 ))];
+    // }
+    // // Create the anchor element
+    // var anchorElement = context.createAnchorElement( 24 + scale[ 0 ] );
+    // anchorElement.connect( filter );
+    // // Sequence
+    // var ms = Math.pow( 10, -3 );
+    // var timeoutLength = 300 * ms; // ms
+    // var sixteenthTime = 125 * ms; // ms
+    // var lastScheduledTime = context.currentTime;
+    // var lastScheduledAnchorTime = context.currentTime;
+    // var lastScheduledCoordinates;
+    // var sequence = function( ) {
+    //   setTimeout( sequence, timeoutLength );
+    //   var timeoutStart = context.currentTime;
+    //   var screen = Math.random( );
+    //   while( lastScheduledTime + sixteenthTime < timeoutStart + timeoutLength ) {
+    //     if( lastScheduledAnchorTime + 8 * sixteenthTime < timeoutStart + timeoutLength ) {
+    //       anchorElement.start( lastScheduledAnchorTime + 8 * sixteenthTime );
+    //       lastScheduledAnchorTime += 8 * sixteenthTime;
+    //       d3.select('#grid').selectAll('circle').filter( function( d ) {
+    //         return d.state === 'A';
+    //       }).transition( )
+    //         .delay( ( lastScheduledAnchorTime - context.currentTime ) * 1000 )
+    //         .duration( 1 * sixteenthTime * 1000 )
+    //         .ease( 'cubic-in-out' )
+    //         .attr('r', Number( radius.slice( 0, -1 ) )*0.75 + '%' )
+    //         .style( 'fill', function( d ) {
+    //           return colors[ d.state ][ 1 ];
+    //         })
+    //         .transition( )
+    //         .duration( 3 * sixteenthTime * 1000 )
+    //         .ease( 'bounce' )
+    //         .attr('r', radius)
+    //         .style( 'fill', function( d ) {
+    //           return colors[ d.state ][ 0 ];
+    //         });
+    //     }
+    //     var ids = Object.keys( elements );
+    //     if( ids.length > 0 && screen > 0.1 ) {
+    //       if( !lastScheduledCoordinates ) {
+    //         var choice = ~~( Math.random( ) * ids.length );
+    //         var sphere = d3.select('#grid').selectAll('circle')
+    //           .filter( function( d ) {
+    //             return Number( ids[ choice ] ) === d.id;
+    //           });
+    //         sphere.each( function( d ) {
+    //           lastScheduledCoordinates = d.coordinates;
+    //         });
+    //         elements[ ids[ choice ] ].sequence.start( lastScheduledTime + sixteenthTime );
+    //         lastScheduledTime += sixteenthTime;
+    //         sphere.transition( )
+    //           .delay( ( lastScheduledTime - context.currentTime ) * 1000 )
+    //           .duration( ( sixteenthTime / 2 ) * 1000 )
+    //           .ease( 'cubic-in-out' )
+    //           .attr( 'r', Number( radius.slice( 0, -1 ) ) * 0.75 + '%' )
+    //           .style( 'fill', function( d ) {
+    //             return colors[ d.state ][ 1 ];
+    //           })
+    //           .transition( )
+    //           .duration( ( sixteenthTime / 2 ) * 1000 )
+    //           .ease( 'elastic' )
+    //           .attr( 'r', radius )
+    //           .style( 'fill', function( d ) {
+    //             return colors[ d.state ][ 0 ];
+    //           });
+    //       } else {
+    //         var neighbors = d3.select('#grid').selectAll('circle').filter( function(d) {
+    //           var condition =
+    //             d.coordinates.x === lastScheduledCoordinates.x + 1 && d.coordinates.y === lastScheduledCoordinates.y ||
+    //             d.coordinates.x === lastScheduledCoordinates.x - 1 && d.coordinates.y === lastScheduledCoordinates.y ||
+    //             d.coordinates.x === lastScheduledCoordinates.x && d.coordinates.y === lastScheduledCoordinates.y + 1 ||
+    //             d.coordinates.x === lastScheduledCoordinates.x && d.coordinates.y === lastScheduledCoordinates.y - 1;
+    //           return condition;
+    //         });
+    //         var choice = ~~(Math.random()*neighbors.size( ));
+    //         var id;
+    //         neighbors.each( function( d, i ) {
+    //           if( i === choice && d.state !== 'A' ) {
+    //             id = d.id;
+    //           } else if( i === choice ) {
+    //             id = -1;
+    //           }
+    //         });
+    //         if( id >= 0 ) {
+    //           var sphere = d3.select('#grid').selectAll('circle')
+    //             .filter( function( d ) {
+    //               return id === d.id;
+    //             });
+    //           sphere.each( function( d ) {
+    //             lastScheduledCoordinates = d.coordinates;
+    //           });
+    //           elements[ id ].sequence.start( lastScheduledTime + sixteenthTime );
+    //           lastScheduledTime += sixteenthTime;
+    //           sphere.transition( )
+    //             .delay( ( lastScheduledTime - context.currentTime ) * 1000 )
+    //             .duration( ( sixteenthTime / 2 ) * 1000 )
+    //             .ease( 'cubic-in-out' )
+    //             .attr( 'r', Number( radius.slice( 0, -1 ) ) * 0.75 + '%' )
+    //             .style( 'fill', function( d ) {
+    //               return colors[ d.state ][ 1 ];
+    //             })
+    //             .transition( )
+    //             .duration( ( sixteenthTime / 2 ) * 1000 )
+    //             .ease( 'elastic' )
+    //             .attr( 'r', radius )
+    //             .style( 'fill', function( d ) {
+    //               return colors[ d.state ][ 0 ];
+    //             });
+    //         } else {
+    //           lastScheduledCoordinates = undefined;
+    //           lastScheduledTime += sixteenthTime;
+    //         }
+    //       }
+    //     } else {
+    //       lastScheduledTime += sixteenthTime;
+    //     }
+    //   }
+    // };
+    // sequence( );
   };
   return {
 
